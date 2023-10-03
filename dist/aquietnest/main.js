e4839b9629dec207c2a42a1a912e2f5f2a579887
Paulepcha <paullep550@gmail.com>
2023-10-03 17:13:17 +0530

    My Project

---
+++ dist/aquietnest/main.js
@@ -0,0 +1,118 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+// ----------------------------ROOMS----------------SLIDER----------------
+
+var swiper = new Swiper(".room-slider", {
+    spaceBetween: 20,
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+// --------------TESTIMONIAL SLIDER----------------
+
+// Access the testimonials
+let testSlide = document.querySelectorAll('.testItem');
+// Access the indicators
+let dots = document.querySelectorAll('.dot');
+
+var counter = 0;
+
+// Add click event to the indicators
+function switchTest(currentTest){
+    currentTest.classList.add('active');
+    var testId = currentTest.getAttribute('attr');
+    if(testId > counter){
+        testSlide[counter].style.animation = 'next1 0.5s ease-in forwards';
+        counter = testId;
+        testSlide[counter].style.animation = 'next2 0.5s ease-in forwards';
+    }
+    else if(testId == counter){return;}
+    else{
+        testSlide[counter].style.animation = 'prev1 0.5s ease-in forwards';
+        counter = testId;
+        testSlide[counter].style.animation = 'prev2 0.5s ease-in forwards';
+    }
+    indicators();
+}
+
+// Add and remove active class from the indicators
+function indicators(){
+    for(i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+    dots[counter].className += ' active';
+}
+
+// Code for auto sliding
+function slideNext(){
+    testSlide[counter].style.animation = 'next1 0.5s ease-in forwards';
+    if(counter >= testSlide.length - 1){
+        counter = 0;
+    }
+    else{
+        counter++;
+    }
+    testSlide[counter].style.animation = 'next2 0.5s ease-in forwards';
+    indicators();
+}
+function autoSliding(){
+    deleteInterval = setInterval(timer, 2000);
+    function timer(){
+        slideNext();
+        indicators();
+    }
+}
+autoSliding();
+
+// Stop auto sliding when mouse is over the indicators
+const container = document.querySelector('.indicators');
+container.addEventListener('mouseover', pause);
+function pause(){
+    clearInterval(deleteInterval);
+}
+
+// Resume sliding when mouse is out of the indicators
+container.addEventListener('mouseout', autoSliding);
+
+
+// -----------------------------------WHITE,GREEN,PURPLE,BLUE,YELLOW --ROOM-----------------------------
+
+
+
+
+// ----------------------------------------Gallery--------------------------------
+
+
+
+
